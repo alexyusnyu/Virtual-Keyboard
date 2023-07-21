4f4afaebbe2dabcba2a29999bf6b2a035fe49c8a
Алекс Ш <slayer1649@gmail.com>
2023-07-21 16:15:02 +0300

    Enter, Caps Lock, Backspace buttons update
    Enter, Caps Lock, Backspace buttons update

--- Keyboard/Keyboard.js
+++ Keyboard/Keyboard.js
@@ -30,17 +30,15 @@ const Keyboard = {
         document.body.appendChild(this.elements.main);
 
 
-
-
     },
 
     _createKeys() {
         const fragment = document.createDocumentFragment();
         const keyLayout = [
-            "1", "2", "3","4","5", "6", "7", "8", "9", "0", "backspace",
-            "q", "w", "e","r","t", "y", "u", "i", "o", "p",
-            "caps", "a", "s","d","f", "g", "h", "j", "k", "l", "enter",
-            "done", "z", "x","c","v", "b", "n", "m", ",", ".", "?",
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+            "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
             "space"
         ];
 
@@ -50,14 +48,50 @@ const Keyboard = {
         };
 
         keyLayout.forEach(key => {
-           const keyElement = Document.createElement("button");
-           const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== 1;
+            const keyElement = Document.createElement("button");
+            const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+
+            // Add attributes/classes
+            keyElement.setAttribute("type", "button");
+            keyElement.classList.add("keyboard__key");
+
+            switch (key) {
+                case "backspace":
+                    keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHTML("backspace");
+
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+                        this._triggerEvents("oninput");
+                    });
+
+                    break;
+
+                case "caps":
+                    keyElement.classList.add("keyboard__key--wide", "keyboard__key--activatable");
+                    keyElement.innerHTML = createIconHTML("keyboard_capslock");
+
+                    keyElement.addEventListener("click", () => {
+                        this._toggleCapsLock();
+                        keyElement.classList.toggle("keyboard__key--active", this.properties.capslock);
+                    });
+
+                    break;
+
+                case "enter":
+                    keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHTML("keyboard_return");
+
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value += "\n";
+                        this._triggerEvents("oninput");
+                    });
+
+                    break;
+
+            }
 
         });
-
-
-
-
     },
 
     _triggerEvents(handlerName) {
